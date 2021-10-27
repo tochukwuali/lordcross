97d00185fc628f4c481abe31a5408e045ca474ef
tcee42 <toochukwuali@gmail.com>
2021-10-27 09:02:15 +0100

    fixes url issues

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,19 @@ import Register from "./pages/Register";
 import Study from "./pages/Study";
 import Scholarship from "./pages/Scholarship";
 import Relocation from "./pages/Relocation";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="">
       <Nav />
+      <Switch>
       <Route path="/register" component={Register} exact />
       <Route path="/study" component={Study} exact />
       <Route path="/scholarship" component={Scholarship} exact />
       <Route path="/relocation" component={Relocation} exact />
       <Route path="/" component={Home} exact />
+      </Switch>
       <Footer />
     </div>
   );
